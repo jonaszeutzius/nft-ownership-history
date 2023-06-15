f7fdd00f27327cfc5c0673f619e9d5b1c37cae88
vboxuser <vboxuser@blockspan-ubuntu.myguest.virtualbox.org>
2023-06-15 09:35:21 -0600

    Removed hardcoded values

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import NFTDisplay from './NFTDisplay';
-import NFTTable from './NFTTable';
+import Wrapper from './NFTDisplay';
+import './App.css';
 
-const contractAddress = '0xffc558bc2fef0d4295d7ce95e379518eaa6789e3';
-const tokenId = '253';
+// const contractAddress = '0xffc558bc2fef0d4295d7ce95e379518eaa6789e3';
+// const tokenId = '253';
 
 function App() {
   return (
     <div className="App">
-      <NFTDisplay contractAddress={contractAddress} tokenId={tokenId} />
-      <NFTTable contractAddress={contractAddress} tokenId={tokenId} />
+      <Wrapper/>
     </div>
   );
 }
--- src/NFTDisplay.js
+++ src/NFTDisplay.js
@@ -1,23 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { getSingleNFT } from './api';
+import { getAllTransfers } from './api';
 
-const NFTDisplay = ({ contractAddress, tokenId }) => {
-  const [nft, setNFT] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-
-
-  useEffect(() => {
-    const fetchNFT = async () => {
-      const data = await getSingleNFT(contractAddress, tokenId);
-      setNFT(data);
-      setLoading(false);
-    };
-
-    fetchNFT();
-  }, [contractAddress, tokenId]);
-  console.log('nft',nft)
-  if (loading) return <p>Loading...</p>;
+const NFTDisplay = ({ nft }) => {
+  if (nft === null) return null;
 
   return (
     <div>
@@ -28,4 +14,70 @@ const NFTDisplay = ({ contractAddress, tokenId }) => {
   );
 };
 
-export default NFTDisplay;
+const NFTTable = ({ transfers }) => {
+  if (transfers === null) return null;
+
+  return (
+    <table style={{ width: '100%', marginTop: '20px' }}>
+      <thead>
+        <tr style={{ backgroundColor: '#f2f2f2' }}>
+          <th style={{ padding: '10px', textAlign: 'left' }}>From</th>
+          <th style={{ padding: '10px', textAlign: 'left' }}>To</th>
+          <th style={{ padding: '10px', textAlign: 'left' }}>Transfer Type</th>
+          <th style={{ padding: '10px', textAlign: 'left' }}>Block Timestamp</th>
+          <th style={{ padding: '10px', textAlign: 'left' }}>Quantity</th>
+        </tr>
+      </thead>
+      <tbody>
+        {transfers.map((transfer, index) => (
+          <tr key={index} style={{ backgroundColor: index % 2 === 0 ? '#f2f2f2' : 'white' }}>
+            <td style={{ padding: '10px' }}>{transfer.from_address}</td>
+            <td style={{ padding: '10px' }}>{transfer.to_address}</td>
+            <td style={{ padding: '10px' }}>{transfer.transfer_type}</td>
+            <td style={{ padding: '10px' }}>{transfer.block_timestamp}</td>
+            <td style={{ padding: '10px' }}>{transfer.quantity}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+const Wrapper = () => {
+  const [contractAddress, setContractAddress] = useState('');
+  const [tokenId, setTokenId] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [nft, setNFT] = useState(null);
+  const [transfers, setTransfers] = useState(null);
+
+  const fetchData = async () => {
+    setLoading(true);
+
+    try {
+      const nftData = await getSingleNFT(contractAddress, tokenId);
+      setNFT(nftData);
+
+      const transfersData = await getAllTransfers(contractAddress, tokenId);
+      setTransfers(transfersData);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <h1>NFT Ownership History</h1>
+      <div className='inputContainer'>
+        <input type="text" placeholder="Contract Address" onChange={(e) => setContractAddress(e.target.value)} />
+        <input type="text" placeholder="Token ID" onChange={(e) => setTokenId(e.target.value)} />
+        <button onClick={fetchData}>View Ownership History</button>
+      </div>
+      <NFTDisplay nft={nft} />
+      <NFTTable transfers={transfers} />
+    </div>
+  );
+};
+
+export default Wrapper;
